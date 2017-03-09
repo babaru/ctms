ad18eb92a8b18c28c27329bb6f92cfb05f666beb
babaru <babaru.trinit@gmail.com>
2017-03-09 17:00:59 +0800

    adjust some style of scenario list

--- app/grids/scenario_grid.rb
+++ app/grids/scenario_grid.rb
@@ -14,8 +14,8 @@ class ScenarioGrid
   column("project-actions", header: '') do |asset|
     format(asset.id) do |value|
       [
-        link_to(fa_icon("pencil"), edit_scenario_path(asset), class: 'btn btn-white btn-sm', data: { toggle: 'tooltip', title: t('buttons.edit') }),
-        link_to(fa_icon('trash'), scenario_path(asset), method: :delete, data: { confirm: t('messages.delete_confirmation'), toggle: 'tooltip', title: t('buttons.delete') }, class: 'btn btn-danger btn-sm')
+        link_to(fa_icon("pencil"), edit_scenario_path(asset), class: 'btn btn-white btn-xs', data: { toggle: 'tooltip', title: t('buttons.edit') }),
+        link_to(fa_icon('trash'), scenario_path(asset), method: :delete, data: { confirm: t('messages.delete_confirmation'), toggle: 'tooltip', title: t('buttons.delete') }, class: 'btn btn-danger btn-xs')
       ].join(' ').html_safe
     end
   end

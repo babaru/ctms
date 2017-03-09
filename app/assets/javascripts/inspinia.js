2c9613683bd4dd0592f99604709f951fbf7713ba
babaru <babaru.trinit@gmail.com>
2017-03-09 13:28:33 +0800

    fix one remain ready function

--- app/assets/javascripts/inspinia.js
+++ app/assets/javascripts/inspinia.js
@@ -205,7 +205,7 @@ $(window).bind("resize", function () {
 
 // Local Storage functions
 // Set proper body class and plugins based on user configuration
-$(document).ready(function () {
+$(document).on('turbolinks:load', function () {
     if (localStorageSupport()) {
 
         var collapse = localStorage.getItem("collapse_menu");

b6aa81776301a6826af8d79cbb523eed5cb8fc20
babaru <babaru.trinit@gmail.com>
2017-03-12 21:46:29 +0800

    add login page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @dashboard_plans_grid = DashboardPlanGrid.new do |scope|
       scope.page(params[:page]).joins(:users).where(users: { id: current_user} ).per(20)

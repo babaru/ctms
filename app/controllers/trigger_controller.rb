8a006f9b8883b445da9f15e7e975316ebfe9fb67
babaru <babaru.trinit@gmail.com>
2017-03-14 14:11:26 +0800

    fix trigger path issue

--- app/controllers/trigger_controller.rb
+++ app/controllers/trigger_controller.rb
@@ -1,5 +1,8 @@
 class TriggerController < ApplicationController
   def index
     logger.info request.body
+    respond_to do |format|
+      format.json { render nothing: true }
+    end
   end
 end

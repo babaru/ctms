7d2844697c5210d0913990028d03f2bf182e8313
babaru <babaru.trinit@gmail.com>
2017-03-14 14:28:28 +0800

    fix trigger request authenticity issue

--- app/controllers/trigger_controller.rb
+++ app/controllers/trigger_controller.rb
@@ -1,8 +1,11 @@
 class TriggerController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  # protect_from_forgery prepend: true, with: :exception
+
   def index
     logger.info request.body
     respond_to do |format|
-      format.json { render nothing: true }
+      format.json { head :ok }
     end
   end
 end

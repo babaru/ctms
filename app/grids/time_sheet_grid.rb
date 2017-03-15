98eeec0a53e88e047b29d69484a17e29d9e599f8
babaru <babaru.trinit@gmail.com>
2017-03-15 09:59:14 +0800

    fix a timesheet grid bug

--- app/grids/time_sheet_grid.rb
+++ app/grids/time_sheet_grid.rb
@@ -5,11 +5,11 @@ class TimeSheetGrid
     TimeSheet.order('time_sheets.id asc')
   end
 
-  1.upto.31 do |n|
-    column(n.to_s, header: n) do |asset|
-      format(asset.id) do |value|
-        n
-      end 
-    end
-  end
+  # 1.upto.31 do |n|
+  #   column(n.to_s, header: n) do |asset|
+  #     format(asset.id) do |value|
+  #       n
+  #     end
+  #   end
+  # end
 end

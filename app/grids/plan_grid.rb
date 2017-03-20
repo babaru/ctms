20cb5e5456ccbc12fc312461ba727f9715f3e447
babaru <babaru.trinit@gmail.com>
2017-03-20 22:00:16 +0800

    hide complete buttons

--- app/grids/plan_grid.rb
+++ app/grids/plan_grid.rb
@@ -5,11 +5,11 @@ class PlanGrid
     Plan.order('plans.id desc')
   end
 
-  column("project-status", header: I18n.t('activerecord.attributes.plan.state')) do |asset|
-    format(asset.state) do |value|
-      plan_state_label(asset)
-    end
-  end
+  # column("project-status", header: I18n.t('activerecord.attributes.plan.state')) do |asset|
+  #   format(asset.state) do |value|
+  #     plan_state_label(asset)
+  #   end
+  # end
 
   column("name project-title", header: I18n.t('activerecord.attributes.plan.title')) do |asset|
     format(asset.title) do |value|
@@ -37,8 +37,7 @@ class PlanGrid
   column("project-actions", header: '') do |asset|
     format(asset.id) do |value|
       [
-        complete_plan_button(asset),
-        watch_plan_button(asset)
+        # complete_plan_button(asset),
       ].join(' ').html_safe
     end
   end
@@ -46,6 +45,7 @@ class PlanGrid
   column("project-actions", header: '') do |asset|
     format(asset.id) do |value|
       [
+        watch_plan_button(asset),
         link_to(fa_icon("pencil"), edit_plan_path(asset), class: 'btn btn-white btn-sm', data: { toggle: 'tooltip', title: t('buttons.edit') }),
         link_to(fa_icon('trash'), plan_path(asset), method: :delete, data: { confirm: t('messages.delete_confirmation'), toggle: 'tooltip', title: t('buttons.delete') }, class: 'btn btn-danger btn-sm')
       ].join(' ').html_safe
--- app/grids/round_grid.rb
+++ app/grids/round_grid.rb
@@ -5,11 +5,11 @@ class RoundGrid
     Round
   end
 
-  column("project-status", header: I18n.t('activerecord.attributes.round.state')) do |asset|
-    format(asset.state) do |value|
-      round_state_label(asset)
-    end
-  end
+  # column("project-status", header: I18n.t('activerecord.attributes.round.state')) do |asset|
+  #   format(asset.state) do |value|
+  #     round_state_label(asset)
+  #   end
+  # end
 
   column("name project-title", header: I18n.t('activerecord.attributes.round.title')) do |asset|
     format(asset.title) do |value|
@@ -29,7 +29,7 @@ class RoundGrid
   column("project-actions", header: '') do |asset|
     format(asset.id) do |value|
       [
-        complete_round_button(asset),
+        # complete_round_button(asset),
         link_to(fa_icon("pencil"), edit_round_path(asset), class: 'btn btn-white btn-sm', data: { toggle: 'tooltip', title: t('buttons.edit') }),
         link_to(fa_icon('trash'), round_path(asset), method: :delete, data: { confirm: t('messages.delete_confirmation'), toggle: 'tooltip', title: t('buttons.delete') }, class: 'btn btn-danger btn-sm')
       ].join(' ').html_safe
